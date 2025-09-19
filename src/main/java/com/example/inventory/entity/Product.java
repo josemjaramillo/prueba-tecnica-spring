03948a6eb6b1ab7bb9445cbbfe3327de1d6f6909
Jose M Jaramillo <jmjaramillo.ing@gmail.com>
2025-09-19 08:34:12 -0500

    First commit

---
+++ src/main/java/com/example/inventory/entity/Product.java
@@ -0,0 +1,39 @@
+package com.example.inventory.entity;
+
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(length = 100)
+    private String description;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    // Constructor vacío obligatorio para JPA
+    public Product() {}
+
+    // Constructor útil
+    public Product(String name, String description, BigDecimal price, Integer stock) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+    }
+
+
+}
